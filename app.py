23d52ab4ca8de6fdbb680cf2bfd72bc2feb96037
Imran <imranmohad78@gmail.com>
2026-03-12 17:00:38 +0530

    Some changes in app.py

--- app.py
+++ app.py
@@ -483,5 +483,8 @@ def err_500(e):
 #  ENTRY POINT
 # ══════════════════════════════════════════════════════════════
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
